ce435890904a320020fd34402c0db882b2dc8e1f
Joy <kimjoydev@gmail.com>
2024-06-30 19:28:05 +0900

    Refactor: Enum CaseIterable 활용하여 UserDefaultsKey 리터럴값 사용 개선

--- ToBuy/UserDefaultsManager.swift
+++ ToBuy/UserDefaultsManager.swift
@@ -7,60 +7,68 @@
 
 import UIKit
 
+enum UserDefaultKey: String, CaseIterable {
+    case nickname
+    case profileImage
+    case searchKeyWord
+    case joinedDate
+    case likedItemId
+}
+
 class UserDefaultManager {
     static let shared = UserDefaultManager()
     static var nickname: String {
         
         get {
-            return UserDefaults.standard.string(forKey: "nickname") ?? ""
+            return UserDefaults.standard.string(forKey: UserDefaultKey.nickname.rawValue) ?? ""
         }
         
         set {
-            UserDefaults.standard.set(newValue, forKey: "nickname")
+            UserDefaults.standard.set(newValue, forKey: UserDefaultKey.nickname.rawValue)
         }
     }
     
     static var profileImage: Int {
         get{
-            return UserDefaults.standard.integer(forKey: "profileImage") 
+            return UserDefaults.standard.integer(forKey: UserDefaultKey.profileImage.rawValue)
         }
         set {
-            UserDefaults.standard.setValue(newValue, forKey: "profileImage")
+            UserDefaults.standard.setValue(newValue, forKey: UserDefaultKey.profileImage.rawValue)
         }
     }
     
     
     static var searchKeyword: [String] {
         get {
-            return UserDefaults.standard.array(forKey: "searchKeyword") as? [String] ?? []
+            return UserDefaults.standard.array(forKey: UserDefaultKey.searchKeyWord.rawValue) as? [String] ?? []
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "searchKeyword")
+            UserDefaults.standard.set(newValue, forKey: UserDefaultKey.searchKeyWord.rawValue)
         }
     }
     
     static var joinedDate: Date {
         get{
-            return UserDefaults.standard.object(forKey: "joinedDate") as? Date ?? Date()
+            return UserDefaults.standard.object(forKey: UserDefaultKey.joinedDate.rawValue) as? Date ?? Date()
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "joinedDate")
+            UserDefaults.standard.set(newValue, forKey: UserDefaultKey.joinedDate.rawValue)
         }
     }
     
     static var likedItemID: [String] {
         get{
-            return UserDefaults.standard.array(forKey: "likedItemId") as? [String] ?? []
+            return UserDefaults.standard.array(forKey: UserDefaultKey.likedItemId.rawValue) as? [String] ?? []
         }
         set {
-            UserDefaults.standard.setValue(newValue, forKey: "likedItemId")
+            UserDefaults.standard.setValue(newValue, forKey: UserDefaultKey.likedItemId.rawValue)
         }
     }
 }
  extension UserDefaultManager {
 
    func clearUserDefaults() {
-       let keys = ["nickname", "profileImage", "searchKeyword", "joinedDate", "likedItemId"]
+       let keys = UserDefaultKey.allCases.map { $0.rawValue }
 
         for i in keys {
             UserDefaults.standard.removeObject(forKey: i)
